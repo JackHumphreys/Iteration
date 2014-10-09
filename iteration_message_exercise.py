6451f621e3fe120a69a5275d75b4c59b90810f4d
Jack <40618@longroad.ac.uk>
2014-10-09 22:25:56 +0100

    Iteration message exercise

---
+++ iteration_message_exercise.py
@@ -0,0 +1,6 @@
+#Iteration message exercise
+
+msg = input("Please enter a message: ")
+repeat = int(input("Please enter many times do you want your message to repeat: "))
+for count in range(repeat):
+    print(msg)
