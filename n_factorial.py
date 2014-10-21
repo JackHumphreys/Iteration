fe1879e26aa679bb5e7e442e4034b77ca6fbd512
40618 <40618@longroad.ac.uk>
2014-10-21 13:01:45 +0100

    N factorial

--- n_factorial.py
+++ n_factorial.py
@@ -1,8 +1,11 @@
 #N factorial
 n = int(input("Please enter n: "))
-num1 = 1
-num2 = 1
-while num1 != n:
-    num1 = num1 + 1
-    num2 = num1 * num2
-print(num2)
+if n > 0:
+    num1 = 1
+    num2 = 1
+    while num1 != n:
+        num1 = num1 + 1
+        num2 = num1 * num2
+    print("{0} factorial is: {1}".format(n,num2))
+else:
+    print("This number is not valid. Try again.")
