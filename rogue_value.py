1fd07a46b04218c454cb08e7f16c0996fb624c7a
40618 <40618@longroad.ac.uk>
2014-10-21 13:02:10 +0100

    Rogue value

--- rogue_value.py
+++ rogue_value.py
@@ -1,9 +1,14 @@
 #Rogue Value
 new_num = 0
-prev_num = 0
+largest_num = 0
 while new_num >= 0:
-    if new_num >= prev_num:
-        new_num = int(input("Please input a number of a series:"))
-    new_num = (new_num+prev_num)
-print(new_num)
+    new_num = int(input("Please input a number of a series:"))
+    if new_num >= largest_num:
+        largest_num = (new_num)
+    else:
+        largest_num = (largest_num)
+print(largest_num)
+        
+        
+
 
