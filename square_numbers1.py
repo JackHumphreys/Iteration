086066b972386f03ed3a7528e5d5cbf6554236ea
Jack <40618@longroad.ac.uk>
2014-11-06 22:14:01 +0000

    Square number exercise.

--- square_numbers1.py
+++ square_numbers1.py
@@ -4,7 +4,7 @@
 squares = int(input("Please enter the amount of square numbers you want: "))
 rows = squares//5
 count = 1
-while squares =<5: 
+while squares <= 5: 
     for count in range(1,6):
         print("{0:>2}".format(count*count),end=" ")
         count = count+1
